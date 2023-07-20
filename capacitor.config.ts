9f456081cc565a9b6d13648c5534d75b90a4dba2
Evans <evarnel7@gmail.com>
2023-07-20 17:45:14 +0300

    Added select pdf file functionality.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'upload',
+  appId: 'com.ionic.upload',
+  appName: 'Upload',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FilePicker } from '@capawesome/capacitor-file-picker';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public filename!: string;
+
   constructor() {}
+
+  public pickFiles = async () => {
+    const result = await FilePicker.pickFiles({
+      types: ['application/pdf'],
+      multiple: false,
+    });
+    this.filename = result.files[0].name;
+    console.log(this.filename);
+    console.log(result.files[0]);
+  };
 
 }
